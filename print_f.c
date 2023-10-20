3a9a19a3214fed3d94fccbe4c30089012e4afc04
hope14657262 <hopemelese89@gmail.com>
2023-10-19 18:06:29 -0700

    0x11. C - printf

--- print_f.c
+++ print_f.c
@@ -1,45 +1,54 @@
-#include <stdio.h>
-#include <stdarg.h>
-
+#include "main.h"
+/**
+ * _printf - printf function
+ * @format: printed
+ * Return: nothing
+ */
 int _printf(const char *format, ...)
 {
-va_list args;
-va_start(args, format);
-
-int chars_printed = 0;
-
-for(int i = 0; format[i] != '\0'; i++)
+int i, printed = 0, printed_chars = 0;
+int flag, width, precision, size, buff_ind = 0;
+va_list list;
+char buffer[BUFF_SIZE];
+if (format == NULL)
+return (1);
+va_start(list, format);
+for (i = 0; format && format[i] != '\0'; i++)
 {
-	if(format[i] == '%')
-	{
-		i++;
-		switch(format[i]
-		{
-			case 'c':
-				chars_printed += printf("%c", va_arg(args, int));
-				break;
-			case 's':
-				chars_printed += printf("%s", va_arg(args, char*)); 
-				break;
-			case '%':
-				chars_printed += printf("%%");
-				break;
-			default:
-				break;
-		}
-	} else {
-		printf("%c", format[i]);
-		chars_printed++;
-	}
+if (format[i] != '0')
+{
+buffer[buff_index] = format[i];
+if (buff_ind == BUFF_SIZE)
+print_buffer(buffer, &buff_ind);
+/*write(1, &format[i], 1);*/
+printed_chars++;
 }
-
-va_end(args);
-return chars_printed;
+else
 }
-
-int main(void) {
-int chars_printed = _printf("Hello %s%c\n", "World", '!');
-printf("Chars printed: %d\n", chars_printed);
-  
-return 0;
+print_buffer(buffer, &buff_ind);
+flags = get_flags(format, &i);
+width = get_width(format, &i, list);
+size = get_size(format, &i);
+++i;
+printed = handle_print(format, &i, list, buffer, flags,
+width, precision, size);
+if (printed == -1)
+return (-1);
+printed_chars += printed;
+}
+}
+print_buffer(buffer, &buff_ind);
+va_end(list);
+return (printed_chars);
+}
+/**
+ * print_buffer - prints the conntentof buffer
+ * @buffer: array of char
+ * @buff_ind: index
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+if (*buff_ind > 0)
+write(1, &buffer[0], *buff_ind);
+*buff_ind = 0;
 }
